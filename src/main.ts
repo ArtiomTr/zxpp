88378606033fa7494710bcbd55437b3b0b06e1eb
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-07-17 13:24:01 +0300

    Added-debugging-option

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { runScriptFromUrl } from './runScriptFromUrl';
 import { runScriptInFile } from './runScriptInFile';
 
 export const main = async () => {
+    if (argv.debug) {
+        console.log('Running zxpp with arguments:', argv, process.argv);
+    }
     const firstArgument = process.argv[2];
 
     if (firstArgument === undefined || firstArgument[0] === '-') {
